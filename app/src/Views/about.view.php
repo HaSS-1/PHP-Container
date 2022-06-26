8bcaec68b82970833cd102ba7e09b5e2980ea82f
HaSSaN <85417909+HaSS-1@users.noreply.github.com>
2022-06-26 20:09:29 +0200

    add about view

---
+++ app/src/Views/about.view.php
@@ -0,0 +1,20 @@
+<?php
+   require APPROOT . '/views/includes/head.php';
+?>
+
+<div class="navbar">
+    <?php
+       require APPROOT . '/views/includes/navigation.php';
+    ?>
+</div>
+
+<h1>About</h1>
+
+<h2>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis voluptatibus minus ratione libero voluptate vel officiis nulla possimus ullam tempore sit optio odio, ipsam debitis cupiditate ipsum, voluptates doloremque dignissimos!
+</h2>
+
+
+
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>
